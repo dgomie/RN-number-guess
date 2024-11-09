2dfa3aec41b37da8e7bbf22f7e9ec91567ef07fc
Danny <danny.f.gomez@gmail.com>
2024-11-08 20:07:36 -0500

    add drop shadows to inputContainer

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -13,8 +13,15 @@ export default function StartGameScreen() {
 
 const styles = StyleSheet.create({
     inputContainer: {
-        flex: 1,
         padding: 16,
         marginTop: 100,
+        marginHorizontal: 24,
+        borderRadius: 8,
+        elevation: 4,
+        shadowColor: "black",
+        shadowOffset: {width: 0, height: 6},
+        shadowRadius: 6,
+        shadowOpacity: .4,
+        backgroundColor: "#72063c",
     }
 })
