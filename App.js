1b87154bf14e00ab080eb70d90c65b45084d91f3
Danny <danny.f.gomez@gmail.com>
2024-11-09 20:11:34 -0500

    add background image and add image styling

--- App.js
+++ App.js
@@ -6,7 +6,12 @@ import StartGameScreen from './screens/StartGameScreen';
 export default function App() {
   return (
     <LinearGradient colors={['#4e0329', '#ddb52f']} style={styles.rootScreen}>
-      <ImageBackground source={}>
+      <ImageBackground
+        source={require('./assets/images/background.png')}
+        resizeMode="cover"
+        style={styles.rootScreen}
+        imageStyle={styles.backgroundImage}
+      >
         <StartGameScreen />
       </ImageBackground>
     </LinearGradient>
@@ -17,4 +22,7 @@ const styles = StyleSheet.create({
   rootScreen: {
     flex: 1,
   },
+  backgroundImage: {
+    opacity: 0.15,
+  }
 });
