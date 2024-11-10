7e572a2a77445cfff34234ec9804e3b0e217338b
Danny <danny.f.gomez@gmail.com>
2024-11-09 20:05:18 -0500

    import imagebackground component

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
-
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, ImageBackground } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 
 import StartGameScreen from './screens/StartGameScreen';
 
 export default function App() {
   return (
-    <LinearGradient colors={["#4e0329", "#ddb52f"]} style={styles.rootScreen}>
-    <StartGameScreen />
+    <LinearGradient colors={['#4e0329', '#ddb52f']} style={styles.rootScreen}>
+      <ImageBackground source={}>
+        <StartGameScreen />
+      </ImageBackground>
     </LinearGradient>
   );
 }
 
-
 const styles = StyleSheet.create({
   rootScreen: {
     flex: 1,
-  }
-})
+  },
+});
