a2a77d608613eb078067b2782a5ae48d28d1c3c4
Danny <danny.f.gomez@gmail.com>
2024-11-09 19:31:36 -0500

    style buttons to show effect when button is pressed

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,9 +1,48 @@
-import { View, Text } from 'react-native';
+import { View, Text, Pressable, StyleSheet } from 'react-native';
 
 export default function PrimaryButton({ children }) {
+  function pressHandler() {
+    console.log('Pressed');
+  }
+
   return (
-    <View>
-      <Text>{children}</Text>
+    <View style={styles.buttonOuterContainer}>
+      <Pressable
+        onPress={pressHandler}
+        android_ripple={{ color: '#640233' }}
+        style={({ pressed }) =>
+          pressed
+            ? [styles.buttonInnerContainer, styles.pressed]
+            : styles.buttonInnerContainer
+        }
+      >
+        <Text style={styles.buttonText}>{children}</Text>
+      </Pressable>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  buttonOuterContainer: {
+    borderRadius: 28,
+    margin: 4,
+    overflow: 'hidden',
+  },
+  buttonInnerContainer: {
+    backgroundColor: '#72063c',
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    elevation: 2,
+    shadowColor: 'black',
+    shadowOffset: 1,
+    shadowOpacity: 0.25,
+    shadowRadius: 1,
+  },
+  buttonText: {
+    color: 'white',
+    textAlign: 'center',
+  },
+  pressed: {
+    opacity: 0.75,
+  },
+});
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -28,7 +28,7 @@ const styles = StyleSheet.create({
     shadowOffset: { width: 0, height: 6 },
     shadowRadius: 6,
     shadowOpacity: 0.4,
-    backgroundColor: '#72063c',
+    backgroundColor: '#4e0329',
   },
   numberInput: {
     height: 50,
