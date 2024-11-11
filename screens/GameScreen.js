40af2d46e9f63fedbd0f4efa1b872bec871d0c85
Danny <danny.f.gomez@gmail.com>
2024-11-11 17:41:20 -0500

    create rand number function

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,6 +1,16 @@
 import { Text, View, StyleSheet } from 'react-native';
 import Title from '../components/Title';
 
+function generateRandomBetween(min, max, exclude){
+    const randNum = Math.floor(Math.random() * (max - min)) + min;
+
+    if (randNum === exclude) {
+        return generateRandomBetween(min, max, exclude);
+    } else {
+        return randNum;
+    }
+}
+
 export default function GameScreen() {
   return (
     <View style={styles.screen}>
@@ -8,7 +18,7 @@ export default function GameScreen() {
       <View>
         <Text>Higher or Lower?</Text>
       </View>
-      {/* <View>Log Rounds</View> */}
+       {/*<View>Log Rounds</View> */}
     </View>
   );
 }
