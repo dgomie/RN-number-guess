cf2f3a344519c24c3d3ec8843d11f4cae3e6685b
Danny <danny.f.gomez@gmail.com>
2024-11-11 18:29:52 -0500

    create new folders within components and update imports

--- components/ui/Title.js
+++ components/ui/Title.js
@@ -1,5 +1,5 @@
 import { Text, StyleSheet } from 'react-native';
-import Colors from '../util/colors';
+import Colors from '../../util/colors';
 
 export default function Title({ children }) {
   return <Text style={styles.title}>{children}</Text>;
--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,24 +1,29 @@
 import { Text, View, StyleSheet } from 'react-native';
-import Title from '../components/Title';
+import Title from '../components/ui/Title';
+import { useState } from 'react';
 
-function generateRandomBetween(min, max, exclude){
-    const randNum = Math.floor(Math.random() * (max - min)) + min;
+function generateRandomBetween(min, max, exclude) {
+  const randNum = Math.floor(Math.random() * (max - min)) + min;
 
-    if (randNum === exclude) {
-        return generateRandomBetween(min, max, exclude);
-    } else {
-        return randNum;
-    }
+  if (randNum === exclude) {
+    return generateRandomBetween(min, max, exclude);
+  } else {
+    return randNum;
+  }
 }
 
-export default function GameScreen() {
+export default function GameScreen({userNumber}) {
+  const initialGuess = generateRandomBetween(1, 100, userNumber);
+  const [currentGuess, setCurrentGuess] = useState(initialGuess);
+
   return (
     <View style={styles.screen}>
       <Title>Opponent's Guess</Title>
+      <Text></Text>
       <View>
         <Text>Higher or Lower?</Text>
       </View>
-       {/*<View>Log Rounds</View> */}
+      {/*<View>Log Rounds</View> */}
     </View>
   );
 }
