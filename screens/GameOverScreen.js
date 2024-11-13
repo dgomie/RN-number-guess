5ef2ba68f12ece88c20f00173faf198e311792da
Danny <danny.f.gomez@gmail.com>
2024-11-13 11:18:43 -0500

    begin layout of game over screen, add styling to containers

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,6 +1,40 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image } from 'react-native';
 import Title from '../components/ui/Title';
+import Colors from '../util/colors'
 
 export default function GameOverScreen() {
-  return <Title>Game Over</Title>;
+  return (
+    <View style={styles.rootContainer}>
+      <Title>GAME OVER</Title>
+      <View style={styles.imageContainer}>
+        <Image source={require('../assets/images/success.png')} style={styles.image}/>
+      </View>
+      <View>
+        <Text>Your phone needed X rounds to guess the number Y</Text>
+      </View>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    padding: 24,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  imageContainer: {
+    borderRadius: 150,
+    borderWidth: 3,
+    borderColor: Colors.primary800,
+    width: 300,
+    height: 300,
+    overflow: "hidden",
+    margin: 36,
+    alignItems: 'center'
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+  }
+});
